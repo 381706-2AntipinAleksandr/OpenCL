bdeeaee6e69a37b132ca16ea8d196de546d327c8
Alex Frog <aleksandr.antipin.1999@mail.ru>
2020-12-08 13:28:11 +0300

    matrix m*n

--- modules/ThirdLab/src/kernels/kernel.cl
+++ modules/ThirdLab/src/kernels/kernel.cl
@@ -1,45 +1,43 @@
 //__constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
 
-__kernel void MatrixMultiplication(__global float* a, __global float* b, __global float* c, const unsigned int size) {
+__kernel void MatrixMultiplication(__global float* a, __global float* b, __global float* c, const unsigned int m, const unsigned int n) {
     int tid_x = get_global_id(0);
     int tid_y = get_global_id(1);
 
-    float res = 0;
-    for (int i = 0; i < size; ++i)
-        res += a[size * tid_y + i] * b[size * i + tid_x];
+    float res = 0.0;
+    for (int i = 0; i < m; ++i)
+        res += a[m * tid_y + i] * b[n * i + tid_x];
 
-    c[size * tid_y + tid_x] = res;
+    c[n * tid_y + tid_x] = res;
 }
 
-__kernel void MatrixMultiplicationBlock(__global float* a, __global float* b, __global float* c, const unsigned int size) {
+__kernel void MatrixMultiplicationBlock(__global float* a, __global float* b, __global float* c, const unsigned int m, const unsigned int n) {
     int tid_x = get_global_id(0);
     int tid_y = get_global_id(1);
     int loc_size_x = get_local_size(0);
     int loc_size_y = get_local_size(1);
     int loc_id_x = get_local_id(0);
     int loc_id_y = get_local_id(1);
-    //int global_group_id_x = get_group_id(0);
-    //int global_group_id_y = get_group_id(1);
 
     __local float A[16 * 16];
     __local float B[16 * 16];
     float res = 0.0;
 
-    for (int p = 0; p < size / loc_size_x; ++p) {
+    for (int p = 0; p < m / loc_size_x; ++p) {
         //A[loc_id_y * loc_size_x + loc_id_x] = a[(global_group_id_y * loc_size_x + loc_id_y) * size + p * loc_size_x + loc_id_x];
         //B[loc_id_y * loc_size_x + loc_id_x] = b[(p * loc_size_y + loc_id_y) * size + global_group_id_x * loc_size_x + loc_id_x];
-        A[loc_id_y * loc_size_x + loc_id_x] = a[tid_y * size + p * loc_size_x + loc_id_x];
-        B[loc_id_y * loc_size_x + loc_id_x] = b[(p * loc_size_y + loc_id_y) * size + tid_x];
+        A[loc_id_y * loc_size_x + loc_id_x] = a[tid_y * m + p * loc_size_x + loc_id_x];
+        B[loc_id_y * loc_size_x + loc_id_x] = b[(p * loc_size_y + loc_id_y) * n + tid_x];
         barrier(CLK_LOCAL_MEM_FENCE);
         for (int i = 0; i < loc_size_x; ++i)
             res += A[loc_size_x * loc_id_y + i] * B[loc_size_y * i + loc_id_x];
         barrier(CLK_LOCAL_MEM_FENCE);
     }
 
-    c[size * tid_y + tid_x] = res;
+    c[n * tid_y + tid_x] = res;
 }
 
-__kernel void MatrixMultiplicationImage(__read_only image2d_t a, __read_only image2d_t b, __write_only image2d_t c, const unsigned int size) {
+__kernel void MatrixMultiplicationImage(__read_only image2d_t a, __read_only image2d_t b, __write_only image2d_t c, const unsigned int m, const unsigned int n) {
     int tid_x = get_global_id(0);
     int tid_y = get_global_id(1);
     int2 grid_id_c = (int2)(tid_x, tid_y);
@@ -49,7 +47,7 @@ __kernel void MatrixMultiplicationImage(__read_only image2d_t a, __read_only ima
     //printf("res - %v4hlf,\t x - %d, y - %d\n", a_loc, tid_x, tid_y);
 
     float4 res = (float4)(0.0f);
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < m; ++i) {
         float4 a_loc = read_imagef(a, sampler, (int2)(i, tid_y));
         float4 b_loc = read_imagef(b, sampler, (int2)(tid_x, i));
         res += a_loc * b_loc;
--- modules/ThirdLab/src/main.cpp
+++ modules/ThirdLab/src/main.cpp
@@ -7,17 +7,18 @@
 #include <sstream>
 #include "../../../init.h"
 
-void SequentialMatrixMultiplication(const size_t n, const float* a, const float* b, float* c) {
+void SequentialMatrixMultiplication(const float* a, const float* b, float* c, const size_t m, const size_t n) {
     for (size_t i = 0; i < n; ++i)
         for (size_t j = 0; j < n; ++j)
         {
-            c[i * n + j] = 0;
-            for (size_t k = 0; k < n; ++k)
-                c[i * n + j] += a[i * n + k] * b[k * n + j];
+            float res = 0.0;
+            for (size_t k = 0; k < m; ++k)
+                res += a[i * m + k] * b[k * n + j];
+            c[i * n + j] = res;
         }
 }
 
-void ParallelMatrixMultiplication(const size_t n, const float* a, const float* b, float* c) {
+void ParallelMatrixMultiplication(const float* a, const float* b, float* c, const size_t m, const size_t n) {
     int num_tr = omp_get_max_threads();
     num_tr = n < num_tr ? n : num_tr;
 #pragma omp parallel num_threads(num_tr)
@@ -26,77 +27,81 @@ void ParallelMatrixMultiplication(const size_t n, const float* a, const float* b
         for (long long i = 0; i < n; ++i)
             for (size_t j = 0; j < n; ++j)
             {
-                c[i * n + j] = 0;
-                for (size_t k = 0; k < n; ++k)
-                    c[i * n + j] += a[i * n + k] * b[k * n + j];
+                float res = 0.0;
+                for (size_t k = 0; k < m; ++k)
+                    res += a[i * m + k] * b[k * n + j];
+                c[i * n + j] = res;
             }
     }
 }
 
-void ParallelOpenCLMatrixMultiplication(const char* path, const size_t group, const unsigned int size,
-                                        float* a, float* b, float* c) {
+void ParallelOpenCLMatrixMultiplication(const char* path, const size_t group, float* a, 
+                                        float* b, float* c, const unsigned int m, const unsigned int n) {
     size_t group_size[] = { group, group };
-    size_t size_n[] = { size, size };
+    size_t size_n[] = { n, n };
     OCLInitialization pr(path, 2, group_size, size_n);
     pr.AddKernel("MatrixMultiplication");
-    pr.AddBuffer<float>(CL_MEM_READ_WRITE, size * size);
-    pr.AddBuffer<float>(CL_MEM_READ_WRITE, size * size);
-    pr.AddBuffer<float>(CL_MEM_WRITE_ONLY, size * size);
-    pr.WriteElementsToBuffer(0, size * size, a);
-    pr.WriteElementsToBuffer(1, size * size, b);
+    pr.AddBuffer<float>(CL_MEM_READ_WRITE, n * m);
+    pr.AddBuffer<float>(CL_MEM_READ_WRITE, m * n);
+    pr.AddBuffer<float>(CL_MEM_WRITE_ONLY, n * n);
+    pr.WriteElementsToBuffer(0, n * m, a);
+    pr.WriteElementsToBuffer(1, m * n, b);
     pr.SetKernelArg(pr.GetKernel(0), 0, &pr.GetBuffer(0));
     pr.SetKernelArg(pr.GetKernel(0), 1, &pr.GetBuffer(1));
     pr.SetKernelArg(pr.GetKernel(0), 2, &pr.GetBuffer(2));
-    pr.SetKernelArg(pr.GetKernel(0), 3, &size);
+    pr.SetKernelArg(pr.GetKernel(0), 3, &m);
+    pr.SetKernelArg(pr.GetKernel(0), 4, &n);
     double start = omp_get_wtime();
     pr.ExecuteKernel(pr.GetKernel(0));
     double end = omp_get_wtime();
     std::cout << "GPU float time - " << end - start << std::endl;
-    pr.ReadElementsFromBuffer(2, size * size, c);
+    pr.ReadElementsFromBuffer(2, n * n, c);
 }
 
-void ParallelOpenCLMatrixMultiplicationOptimized(const char* path, const size_t group, const unsigned int size,
-                                                 float* a, float* b, float* c) {
+void ParallelOpenCLMatrixMultiplicationOptimized(const char* path, const size_t group, float* a,
+                                                 float* b, float* c, const unsigned int m, const unsigned int n) {
     size_t group_size[] = { group, group };
-    size_t size_n[] = { size, size };
+    size_t size_n[] = { n, n };
     OCLInitialization pr(path, 2, group_size, size_n);
     pr.AddKernel("MatrixMultiplicationBlock");
-    pr.AddBuffer<float>(CL_MEM_READ_WRITE, size * size);
-    pr.AddBuffer<float>(CL_MEM_READ_WRITE, size * size);
-    pr.AddBuffer<float>(CL_MEM_WRITE_ONLY, size * size);
-    pr.WriteElementsToBuffer(0, size * size, a);
-    pr.WriteElementsToBuffer(1, size * size, b);
+    pr.AddBuffer<float>(CL_MEM_READ_WRITE, n * m);
+    pr.AddBuffer<float>(CL_MEM_READ_WRITE, m * n);
+    pr.AddBuffer<float>(CL_MEM_WRITE_ONLY, n * n);
+    pr.WriteElementsToBuffer(0, n * m, a);
+    pr.WriteElementsToBuffer(1, m * n, b);
     pr.SetKernelArg(pr.GetKernel(0), 0, &pr.GetBuffer(0));
     pr.SetKernelArg(pr.GetKernel(0), 1, &pr.GetBuffer(1));
     pr.SetKernelArg(pr.GetKernel(0), 2, &pr.GetBuffer(2));
-    pr.SetKernelArg(pr.GetKernel(0), 3, &size);
+    pr.SetKernelArg(pr.GetKernel(0), 3, &m);
+    pr.SetKernelArg(pr.GetKernel(0), 4, &n);
     double start = omp_get_wtime();
     pr.ExecuteKernel(pr.GetKernel(0));
     double end = omp_get_wtime();
     std::cout << "GPU optimized float time - " << end - start << std::endl;
-    pr.ReadElementsFromBuffer(2, size * size, c);
+    pr.ReadElementsFromBuffer(2, n * n, c);
 }
 
-void ParallelOpenCLMatrixMultiplicationImage(const char* path, const size_t group, const unsigned int size,
-                                             float* a, float* b, float* c) {
+void ParallelOpenCLMatrixMultiplicationImage(const char* path, const size_t group, float* a, 
+                                             float* b, float* c, const unsigned int m, const unsigned int n) {
     size_t group_size[] = { group, group, 1 };
-    size_t size_n[] = { size, size, 1 };
+    size_t size_n[] = { n, n, 1 };
     OCLInitialization pr(path, 3, group_size, size_n);
     pr.AddKernel("MatrixMultiplicationImage");
-    pr.AddImage<float>(CL_MEM_READ_ONLY, size, size);
-    pr.AddImage<float>(CL_MEM_READ_ONLY, size, size);
-    pr.AddImage<float>(CL_MEM_WRITE_ONLY, size, size);
-    pr.WriteElementsToImage(0, size, size, a);
-    pr.WriteElementsToImage(1, size, size, b);
+    pr.AddImage<float>(CL_MEM_READ_ONLY, m, n);
+    pr.AddImage<float>(CL_MEM_READ_ONLY, n, m);
+    pr.AddImage<float>(CL_MEM_WRITE_ONLY, n, n);
+    pr.WriteElementsToImage(0, m, n, a);
+    pr.WriteElementsToImage(1, n, m, b);
     pr.SetKernelArg(pr.GetKernel(0), 0, &pr.GetImage(0));
     pr.SetKernelArg(pr.GetKernel(0), 1, &pr.GetImage(1));
     pr.SetKernelArg(pr.GetKernel(0), 2, &pr.GetImage(2));
-    pr.SetKernelArg(pr.GetKernel(0), 3, &size);
+    pr.SetKernelArg(pr.GetKernel(0), 3, &m);
+    pr.SetKernelArg(pr.GetKernel(0), 4, &n);
     double start = omp_get_wtime();
     pr.ExecuteKernel(pr.GetKernel(0));
     double end = omp_get_wtime();
     std::cout << "GPU optimized by image float time - " << end - start << std::endl;
-    pr.ReadElementsFromImage(2, size, size, c);
+    pr.ReadElementsFromImage(2, n, n, c);
 }
 
 bool IsEqual(const float* a, const float* b, const size_t n) {
@@ -112,59 +117,65 @@ bool IsEqual(const float* a, const float* b, const size_t n) {
 
 
 int main(int argc, char** argv) {
-    const unsigned int size = 1024;
+    //const unsigned int size = 1024;
+    const unsigned int m = 2048;
+    const unsigned int n = 1024;
 
     // init float
-    float* a_seq = new float[size * size];
-    float* b_seq = new float[size * size];
-    float* c_seq = new float[size * size];
+    float* a_seq = new float[n * m];
+    float* b_seq = new float[n * m];
+    float* c_seq = new float[n * n];
 
-    float* a_par_cpu = new float[size * size];
-    float* b_par_cpu = new float[size * size];
-    float* c_par_cpu = new float[size * size];
+    float* a_par_cpu = new float[n * m];
+    float* b_par_cpu = new float[n * m];
+    float* c_par_cpu = new float[n * n];
 
-    float* a_par_gpu = new float[size * size];
-    float* b_par_gpu = new float[size * size];
-    float* c_par_gpu = new float[size * size];
+    float* a_par_gpu = new float[n * m];
+    float* b_par_gpu = new float[n * m];
+    float* c_par_gpu = new float[n * n];
 
-    float* a_par_gpu_opt = new float[size * size];
-    float* b_par_gpu_opt = new float[size * size];
-    float* c_par_gpu_opt = new float[size * size];
+    float* a_par_gpu_opt = new float[n * m];
+    float* b_par_gpu_opt = new float[n * m];
+    float* c_par_gpu_opt = new float[n * n];
 
-    float* a_par_gpu_img = new float[size * size];
-    float* b_par_gpu_img = new float[size * size];
-    float* c_par_gpu_img = new float[size * size];
+    float* a_par_gpu_img = new float[n * m];
+    float* b_par_gpu_img = new float[n * m];
+    float* c_par_gpu_img = new float[n * n];
 
     float lower_bound = -10.0;
     float upper_bound = 10.0;
     std::cout << "gen start" << std::endl;
     std::uniform_real_distribution<float> unif_f(lower_bound, upper_bound);
     std::default_random_engine re;
-    for (int i = 0; i < size * size; ++i) {
-        float num_f = unif_f(re);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            float num_f = unif_f(re);
 
-        a_seq[i] = a_par_cpu[i] = a_par_gpu[i] = a_par_gpu_opt[i] = a_par_gpu_img[i] = num_f;
+            a_seq[i * n + j] = a_par_cpu[i * n + j] = a_par_gpu[i * n + j] = a_par_gpu_opt[i * n + j]
+                = a_par_gpu_img[i * n + j] = num_f;
 
-        b_seq[i] = b_par_cpu[i] = b_par_gpu[i] = b_par_gpu_opt[i] = b_par_gpu_img[i] = static_cast<float>(10.0) - num_f;
+            b_seq[i * n + j] = b_par_cpu[i * n + j] = b_par_gpu[i * n + j] = b_par_gpu_opt[i * n + j]
+                = b_par_gpu_img[i * n + j] = static_cast<float>(10.0) - num_f;
+        }
     }
     std::cout << "gen end" << std::endl;
 
     double start = omp_get_wtime();
-    SequentialMatrixMultiplication(size, a_seq, b_seq, c_seq);
+    SequentialMatrixMultiplication(a_seq, b_seq, c_seq, m, n);
     double end = omp_get_wtime();
     std::cout << "Sequential float time - " << end - start << std::endl;
     start = omp_get_wtime();
-    ParallelMatrixMultiplication(size, a_par_cpu, b_par_cpu, c_par_cpu);
+    ParallelMatrixMultiplication(a_par_cpu, b_par_cpu, c_par_cpu, m, n);
     end = omp_get_wtime();
     std::cout << "OpenMP float time - " << end - start << std::endl;
-    ParallelOpenCLMatrixMultiplication(argv[0], 16, size, a_par_gpu, b_par_gpu, c_par_gpu);
-    std::cout << "Float compare - " << (IsEqual(c_seq, c_par_gpu, size * size) == true ? "true" : "false") << std::endl;
+    ParallelOpenCLMatrixMultiplication(argv[0], 16, a_par_gpu, b_par_gpu, c_par_gpu, m, n);
+    std::cout << "Float compare - " << (IsEqual(c_seq, c_par_gpu, n * n) == true ? "true" : "false") << std::endl;
 
-    ParallelOpenCLMatrixMultiplicationOptimized(argv[0], 16, size, a_par_gpu_opt, b_par_gpu_opt, c_par_gpu_opt);
-    std::cout << "Float compare optimized - " << (IsEqual(c_seq, c_par_gpu_opt, size * size) == true ? "true" : "false") << std::endl;
+    ParallelOpenCLMatrixMultiplicationOptimized(argv[0], 16, a_par_gpu_opt, b_par_gpu_opt, c_par_gpu_opt, m, n);
+    std::cout << "Float compare optimized - " << (IsEqual(c_seq, c_par_gpu_opt, n * n) == true ? "true" : "false") << std::endl;
 
-    ParallelOpenCLMatrixMultiplicationImage(argv[0], 16, size, a_par_gpu_img, b_par_gpu_img, c_par_gpu_img);
-    std::cout << "Float compare image - " << (IsEqual(c_seq, c_par_gpu_img, size * size) == true ? "true" : "false") << std::endl;
+    ParallelOpenCLMatrixMultiplicationImage(argv[0], 16, a_par_gpu_img, b_par_gpu_img, c_par_gpu_img, m, n);
+    std::cout << "Float compare image - " << (IsEqual(c_seq, c_par_gpu_img, n * n) == true ? "true" : "false") << std::endl;
 
     delete[] a_seq;
     delete[] b_seq;
